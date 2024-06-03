8cb28a2f531b69b1c9136853941e04487edb30fd
nicholas <nicholasvdeabreu@gmail.com>
2024-06-03 12:23:34 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,58 +1,62 @@
-let botao = document.querySelector('.btn-nao');
-let botaoSim = document.getElementById("botao");
+let botaoNao = document.querySelector('.btn-nao');
+let botaoSim = document.getElementById("btn-sim");
 
 document.getElementById("heart").style.display = "none";
 
-
-botao.addEventListener('click', function(){
+botaoNao.addEventListener('mouseover', function() {
     move(this);
 });
 
-//função que faz o botão ficar movendo
-function move(el){  
-    let div = document.getElementsByClassName("container-button");
+// Função que faz o botão ficar movendo
+function move(el) {
+    let div = document.getElementsByClassName("container-button")[0];
 
     let width = Math.ceil(el.clientWidth);
     let height = Math.ceil(el.clientHeight);
 
-    let wdh = Math.floor(div[0].clientHeight);
-    let wdw = Math.floor(div[0].clientWidth);
+    let wdh = Math.floor(div.clientHeight);
+    let wdw = Math.floor(div.clientWidth);
 
-    var changeTop = Math.floor((Math.random() * (wdh - height + 1)));
-    var changeLeft = Math.floor((Math.random() * (wdw - width + 1)));
+    let changeTop = Math.floor((Math.random() * (wdh - height)));
+    let changeLeft = Math.floor((Math.random() * (wdw - width)));
     console.log("Height: " + changeTop + " Width: " + changeLeft);
 
     el.style.marginTop = changeTop + "px";
     el.style.marginLeft = changeLeft + "px";
 }
 
-botaoSim .addEventListener('click', function(){
-    document.getElementById("btn-sim").classList.remove("move");
+botaoSim.addEventListener('click', function() {
+    document.getElementById("botao").classList.remove("move");
 
-    setHeart(this);
-    adicionaDiv()
-    
+    setHeart();
+    showMessage();
+    changeTitle();
 });
 
-function setHeart(element){
+function setHeart() {
     document.getElementById("botao").style.display = "none";
-    document.getElementById("botao2").style.display = "none";
+    document.querySelector('.btn-nao').style.display = "none";
     document.getElementById("heart").style.display = "block";
     playSong();
 }
 
-function playSong(){
+function playSong() {
     let snd = new Audio("love.mp3");
     snd.play();
 }
 
-function adicionaDiv() {
-    document.getElementById("heart")[0]
+function showMessage() {
+    const message = document.createElement('div');
+    message.className = 'message';
+    message.innerText = 'O meu coração é teu';
+    document.body.appendChild(message);
+    message.style.display = 'block';
 }
 
+function changeTitle() {
+    document.getElementById('titulo').innerText = 'Agora vai ter que me aturar para sempre! Eu te amo!';
+}
 
-
-
-
-
-   
+function adicionaDiv() {
+    // Função adicionada para compatibilidade
+}
